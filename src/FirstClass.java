af41632dd139566f5c2c63f1a089f42d565bf976
pyk <cutegirlyk@naver.com>
2022-01-18 18:55:09 +0900

    dfs,bfs 추가

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,81 +1,79 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-/*
-sample input(첫 줄의 첫 숫자는 정점의 개수, 두 번째 숫자는 간선의 개수).
-6 9
-1 6 5
-2 4 6
-1 2 7
-3 5 15
-5 6 9
-3 4 10
-1 3 11
-2 3 3
-4 5 7
- */
+class Node {
+
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
 
 public class FirstClass {
-    static int V, E;
-    static int[][] graph;
-    // 각 노드의 부모
-    static int[] parent;
-    // 최종적으로 연결된 최소 신장 트리 연결 비용.
-    static int final_cost;
+
+    public static int n, m;
+    public static int[][] graph = new int[201][201];
+
+    // 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+    public static int dx[] = {-1, 1, 0, 0};
+    public static int dy[] = {0, 0, -1, 1};
+
+    public static int bfs(int x, int y) {
+        // 큐(Queue) 구현을 위해 queue 라이브러리 사용
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y));
+        // 큐가 빌 때까지 반복하기
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            x = node.getX();
+            y = node.getY();
+            // 현재 위치에서 4가지 방향으로의 위치 확인
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                // 미로 찾기 공간을 벗어난 경우 무시
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                // 벽인 경우 무시
+                if (graph[nx][ny] == 0) continue;
+                // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+                if (graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    q.offer(new Node(nx, ny));
+                }
+            }
+        }
+        // 가장 오른쪽 아래까지의 최단 거리 반환
+        return graph[n - 1][m - 1];
+    }
 
     public static void main(String[] args) {
-        // 그래프의 연결상태(노드1, 노드2, 비용)를 초기화.
         Scanner sc = new Scanner(System.in);
-        V = sc.nextInt();
-        E = sc.nextInt();
-        graph = new int[E][3];
-        for (int i = 0; i < E; i++) {
-            graph[i][0] = sc.nextInt();
-            graph[i][1] = sc.nextInt();
-            graph[i][2] = sc.nextInt();
-        }
-        parent = new int[V];
-        final_cost = 0;
 
-        // 간선 비용 순으로 오름차순 정렬
-        Arrays.sort(graph, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+        // N, M을 공백을 기준으로 구분하여 입력 받기
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine(); // 버퍼 지우기
 
-        // makeSet
-        for (int i = 0; i < V; i++) {
-            parent[i] = i;
-        }
-        // 낮은 비용부터 크루스칼 알고리즘 진행
-        for (int i = 0; i < E; i++) {
-            // 사이클이 존재하지 않는 경우에만 간선을 선택한다(여기서는 최종 비용만 고려하도록 하겠다).
-            if (find(graph[i][0] - 1) != find(graph[i][1] - 1)) {
-                System.out.println("<선택된 간선>");
-                System.out.println(Arrays.toString(graph[i]));
-                union(graph[i][0] - 1, graph[i][1] - 1);
-                final_cost += graph[i][2];
-                System.out.println("<각 노드가 가리키고 있는 부모>");
-                System.out.println(Arrays.toString(parent) + "\n");
-                continue;
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
             }
         }
 
-        System.out.println("최종 비용 : " + final_cost);
-        sc.close();
+        // BFS를 수행한 결과 출력
+        System.out.println(bfs(0, 0));
     }
 
-    private static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a > b) {
-            parent[a] = b;
-        } else {
-            parent[b] = a;
-        }
-    }
-
-    private static int find(int x) {
-        if (parent[x] == x)
-            return x;
-        else
-            return find(parent[x]);
-    }
 }
